f246cde8397d4e66db372741c48d311da4aa74c5
Victor Randall <victor.randall13@gmail.com>
2021-01-11 16:40:44 -0300

    implemented feature to break blocks

--- config/KinematicBody.gd
+++ config/KinematicBody.gd
@@ -1,11 +1,13 @@
 extends KinematicBody
 
+onready var raycast = $Spatial/RayCast
+var VectorBlock = Vector3()
 var water = 100
 var life = 100
 var speed = 32
 var menu = false
-var jump = 70
-var gravity = 5
+var jump = 90
+var gravity = 1
 var motion = Vector3()
 var mouse_sensitivity = 0.5
 onready var head = $Spatial
@@ -30,16 +32,17 @@ func _input(event):
 			head.rotation.x = clamp(head.rotation.x, deg2rad(-90), deg2rad(90))
 
 func _physics_process(delta):
-	if motion != Vector3():
-		if is_network_master():
-			move_and_slide(motion * speed, Vector3.UP)
-			rpc_unreliable("_set_position", global_transform.origin)
+#	if motion != Vector3():
+#		if is_network_master():
+	move_and_slide(motion, Vector3.UP)
+#			rpc_unreliable("_set_position", global_transform.origin)
 		
 	motion.x = 0
 	motion.z = 0
 	
 	if Input.is_action_pressed("ui_left_click"):
-		pass
+		if raycast.is_colliding():
+			VectorBlock = raycast.get_collision_point()
 		
 	
 	if not is_on_floor():
@@ -65,11 +68,20 @@ func _physics_process(delta):
 		motion += -transform.basis.x * speed
 	
 	if Input.is_action_pressed("ui_sprint"):
-		speed = 16
+		speed = 32
+		$Spatial.translation.y = 1.228
+		$CollisionShape.scale.y = 1
+		$CollisionShape.translation.y = 0
 	elif Input.is_action_pressed("ui_crouch"):
 		speed = 4
+		$Spatial.translation.y = -0.041
+		$CollisionShape.scale.y = 0.7
+		$CollisionShape.translation.y = -0.75
 	else:
 		speed = 8
+		$Spatial.translation.y = 1.228
+		$CollisionShape.scale.y = 1
+		$CollisionShape.translation.y = 0
 		
 	
 	
--- config/blocks.gd
+++ config/blocks.gd
@@ -1,29 +1,55 @@
 extends GridMap
 
+var n = 45
+const GRASS = 0
+const STONE = 2
+const DIRT = 1
+const SAND = 3
+var biomes_selected = 'flat'
 var random = RandomNumberGenerator.new()
-var mutiplier = 15
+var mutiplier = 2
 var divider = 8
-var chunk = 5
+var chunk = 2
 var speed = 10
 onready var player = $KinematicBody
 var noise = OpenSimplexNoise.new()
 #
 func _ready():
+	$Control.hide()
 	random.randomize()
 	noise.seed = random.randi()
 	noise.persistence = 1
 	print("seed:", random.randi())
-	
+	for x in range(-n, n):
+		for y in range(0, 100):
+			for z in range(-n, n):
+					if y < noise.get_noise_2d(x,z)*mutiplier+10:
+						if get_cell_item(x,y,z) == -1:
+							set_cell_item(x,y,z,GRASS)
+							set_cell_item(x,y-1,z,DIRT)
+							set_cell_item(x,y-9,z,STONE)
+							noise.persistence = 20
 
-func create_world(x_trans,y_trans,z_trans,x_size,y_size,z_size):
-	for x in range(x_trans,x_size):
-		for y in range(y_trans,y_size):
-			for z in range(z_trans,z_size):
-				if y < noise.get_noise_2d(x,z)*noise.get_noise_3d(x,y,z)-noise.get_noise_3d(x,y,z)*mutiplier+15:
+func create_world(x_start, y_start, z_start, x_end, y_end, z_end, biomes_name):
+	for x in range(x_start, x_end):
+		for y in range(y_start, y_end):
+			for z in range(z_start, z_end):
+				var biomes = {
+						'mountains': noise.get_noise_2d(x,z)*noise.get_noise_3d(x,y,z),
+						'chaos': noise.get_noise_2d(x,z)-noise.get_noise_3d(x,y,z)+noise.get_noise_3d(x,y,z)*noise.get_noise_3d(x,y,z),
+						'flat': noise.get_noise_2d(x,z),
+						'hills': noise.get_noise_2d(x,z),
+					}
+				if y < biomes[biomes_name]*mutiplier+10:
 					if get_cell_item(x,y,z) == -1:
-						set_cell_item(x,y,z,0)
-					
-					
+						set_cell_item(x,y,z,GRASS)
+						set_cell_item(x,y-1,z,DIRT)
+						set_cell_item(x,y-9,z,STONE)
+						noise.persistence = 10
+						
+						
+						
+						
 #	if not range(x_trans,x_size):
 #		if not range(y_trans,y_size):
 #			if not range(z_trans,z_size):
@@ -31,12 +57,39 @@ func create_world(x_trans,y_trans,z_trans,x_size,y_size,z_size):
 
 
 func _process(delta):
-	create_world(player.translation.x/divider-chunk,0,player.translation.z/divider-chunk,player.translation.x/divider+chunk,100,player.translation.z/divider+chunk)
-	noise.persistence = 20
+#	create_world(player.translation.x/divider-chunk, 0, player.translation.z/divider-chunk, player.translation.x/divider+chunk+1, 100, player.translation.z/divider+chunk+1, biomes_selected)
+	
+	var Block = world_to_map(player.VectorBlock)
+	set_cell_item(Block.x,Block.y,Block.z,-1) 
+	
 	$DirectionalLight.rotation.x += 0.0001
 	if $WorldEnvironment.get_environment().get_sky().sun_latitude == 180:
 		$WorldEnvironment.get_environment().get_sky().sun_latitude = 0
 	else:
 		$WorldEnvironment.get_environment().get_sky().sun_latitude += 0.01
-	if Input.is_action_pressed("ui_cancel"):
-		get_tree().quit()
+	
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		get_tree().paused = true
+		$Control.show()
+		
+	
+	if Input.is_action_just_pressed("ui_test"):
+		if biomes_selected == 'flat':
+			biomes_selected = 'mountains'
+		elif biomes_selected == 'mountains':
+			biomes_selected = 'chaos'
+		elif biomes_selected == 'chaos':
+			biomes_selected = 'hills'
+		else:
+			biomes_selected = 'flat'
+		print(biomes_selected)
+
+
+func _on_Button_pressed():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	get_tree().paused = false
+	$Control.hide()
+
+func _on_Button2_pressed():
+	get_tree().quit()
